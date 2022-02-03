9e09a45daf59bc0a4de8bc1a92bc1978bd0d4e42
SMANDHOL <SMANDHOL@HS-PF1YQ1PT.idc.oracle.com>
2022-02-03 16:43:17 +0530

    commit

--- AuthenticationService/src/main/java/com/authenticationService/controllers/FirstController.java
+++ AuthenticationService/src/main/java/com/authenticationService/controllers/FirstController.java
@@ -1,9 +1,11 @@
 package com.authenticationService.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/MyLuck/v1")
 public class FirstController {
 
 	@GetMapping("/Welcome")
